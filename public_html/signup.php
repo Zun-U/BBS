4fb7d3fba1f9168b579c8f71b3db880dbbe48511
Zun-U <hello.tra.uenishi@gmail.com>
2022-06-13 14:42:09 +0900

    add signup

--- public_html/signup.php
+++ public_html/signup.php
@@ -30,24 +30,30 @@
 </div>
 </header>
 <div class="wrapper">
+  <?php
+  require_once(__DIR__ .'/header.php');
+  $app = new Bbs\Controller\Signup();
+  $app->run();
+  ?>
 <div class="container">
   <form action="thread_all.html" method="post" id="signup" class="form">
     <div class="form-group">
       <label>メールアドレス</label>
-      <input type="text" name="email" value="" class="form-control">
-      <p class="err"></p>
+      <input type="text" name="email" value="<?= isset($app->getValues()->email) ? h($app->getValues()->email): ''; ?>" class="form-control">
+      <p class="err"><?= h($app->getErrors('email')); ?></p>
     </div>
     <div class="form-group">
       <label>ユーザー名</label>
-      <input type="text" name="username" value="" class="form-control">
-      <p class="err"></p>
+      <input type="text" name="username" value="<?= isset($app->username) ? h($app->getValues()->username): ''; ?>" class="form-control">
+      <p class="err"><?= h($app->getErrors('username')); ?></p>
     </div>
     <div class="form-group">
       <label>パスワード</label>
       <input type="password" name="password" class="form-control">
-      <p class="err"></p>
+      <p class="err"><?= h($app->getErrors('password')); ?></p>
     </div>
-    <button class="btn btn-primary">登録</button>
+    <button class="btn btn-primary" oneclick="document.getElementById('signup').submit();">登録</button>
+    <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
   </form>
   <p class="fs12"><a href="./login.php">ログイン</a></p>
 </div><!-- container -->
