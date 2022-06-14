712e3c211f6b7f586f2ba7b68271197f0cfdf4b0
Zun-U <hello.tra.uenishi@gmail.com>
2022-06-14 16:30:34 +0900

    login test

--- lib/Model/User.php
+++ lib/Model/User.php
@@ -19,12 +19,32 @@ class User extends \Bbs\Model
     $res = $stmt->execute([
       ':username' => $values['username'],
       ':email' => $values['email'],
+
       // パスワードのハッシュ化
+      // 「password_hash」　PHPに用意されている命令。パスワードをそのままデータベースに登録しないで、暗号化をしている。
       ':password' => password_hash($values['password'], PASSWORD_DEFAULT)
     ]);
+
     // メールアドレスがユニークでなければfalseを返す
+    // （メールアドレスがすでに登録されている場合）
     if ($res === false) {
+      var_dump($values);
       throw new \Bbs\Exception\DuplicateEmail();
     }
   }
+public function login($values) {
+  $stmt = $this->db->prepare("SELECT * FROM users WHERE email = :email;");
+  $stmt->execute([
+    ':email' => $values['email']
+  ]);
+  $stmt->setFetchMode(\PDO::FETCH_CLASS, 'stdClass');
+  $user = $stmt->fetch();
+  if (empty($user)) {
+    throw new \Bbs\Exception\UnmatchEmailOrPassword();
+  }
+  if (!password_verify($values['password'], $user->password)) {
+    throw new \Bbs\Exception\UnmatchEmailOrPassword();
+  }
+  return $user;
+}
 }
