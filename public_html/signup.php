2a9157807c9a6daed97a09c2cac7c9e41b14312c
Zun-U <hello.tra.uenishi@gmail.com>
2022-06-14 09:33:09 +0900

    add user

--- public_html/signup.php
+++ public_html/signup.php
@@ -30,13 +30,13 @@
 </div>
 </header>
 <div class="wrapper">
-  <?php
-  require_once(__DIR__ .'/header.php');
-  $app = new Bbs\Controller\Signup();
-  $app->run();
-  ?>
+<?php
+require_once(__DIR__ .'/header.php');
+$app = new Bbs\Controller\Signup();
+$app->run();
+?>
 <div class="container">
-  <form action="thread_all.html" method="post" id="signup" class="form">
+  <form action="" method="post" id="signup" class="form">
     <div class="form-group">
       <label>メールアドレス</label>
       <input type="text" name="email" value="<?= isset($app->getValues()->email) ? h($app->getValues()->email): ''; ?>" class="form-control">
@@ -44,7 +44,7 @@
     </div>
     <div class="form-group">
       <label>ユーザー名</label>
-      <input type="text" name="username" value="<?= isset($app->username) ? h($app->getValues()->username): ''; ?>" class="form-control">
+      <input type="text" name="username" value="<?= isset($app->getValues()->username) ? h($app->getValues()->username): ''; ?>" class="form-control">
       <p class="err"><?= h($app->getErrors('username')); ?></p>
     </div>
     <div class="form-group">
@@ -52,11 +52,13 @@
       <input type="password" name="password" class="form-control">
       <p class="err"><?= h($app->getErrors('password')); ?></p>
     </div>
-    <button class="btn btn-primary" oneclick="document.getElementById('signup').submit();">登録</button>
+    <button class="btn btn-primary" onclick="document.getElementById('signup').submit();">登録</button>
     <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
   </form>
-  <p class="fs12"><a href="./login.php">ログイン</a></p>
+  <p class="fs12"><a href="<?= SITE_URL; ?>/login.php">ログイン</a></p>
 </div><!-- container -->
+<?php require_once(__DIR__ .'/footer.php'); ?>
+
 </div> <!-- wrapper -->
 <p class="copy"><small>&copy; 2019 code lab.</small></p>
 <script src="./js/bbs.js"></script>
