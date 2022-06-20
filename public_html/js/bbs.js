0f120f82f2178dd96cfb4ea7bf81bb5edc84fe46
Zun-U <hello.tra.uenishi@gmail.com>
2022-06-20 15:48:00 +0900

    user menu

--- public_html/js/bbs.js
+++ public_html/js/bbs.js
@@ -1,2 +1,23 @@
 $(function () {
+  $('input[type=file]').change(function () {
+    let file = $(this).prop('files')[0];
+
+    // 画像以外は処理を停止
+    if(!file.type.match('image.*')){
+      // クリア
+      $(this).val('');
+      $('.imgfile').html('');
+      return;
+    }
+
+    // 画像表示
+let reader = new FileReader();
+reader.onload = function(){
+  let img_src = $('<img>').attr('src', reader.result);
+  $('.imgfile').html(img_src);
+  $('.imgarea').removeClass('noimage');
+}
+reader.readAsDataURL(file);
+  }
+  );
 });
